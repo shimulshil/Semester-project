c0951fe12f8b38a8ac04adcfe273c397236f9c53
nishant3392 <80105917+nishant3392@users.noreply.github.com>
2021-05-09 08:38:23 +0200

    create signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,50 +2,72 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  
   {
     path: 'room-zone-select',
-    loadChildren: () => import('./pages/room-zone-select/room-zone-select.module').then( m => m.RoomZoneSelectPageModule)
+    loadChildren: () =>
+      import('./pages/room-zone-select/room-zone-select.module').then(
+        (m) => m.RoomZoneSelectPageModule
+      ),
   },
   {
     path: 'zone-status',
-    loadChildren: () => import('./pages/zone-status/zone-status.module').then( m => m.ZoneStatusPageModule)
+    loadChildren: () =>
+      import('./pages/zone-status/zone-status.module').then(
+        (m) => m.ZoneStatusPageModule
+      ),
   },
   {
     path: 'change-clima',
-    loadChildren: () => import('./pages/change-clima/change-clima.module').then( m => m.ChangeClimaPageModule)
+    loadChildren: () =>
+      import('./pages/change-clima/change-clima.module').then(
+        (m) => m.ChangeClimaPageModule
+      ),
   },
   {
     path: 'only-zone',
-    loadChildren: () => import('./pages/only-zone/only-zone.module').then( m => m.OnlyZonePageModule)
+    loadChildren: () =>
+      import('./pages/only-zone/only-zone.module').then(
+        (m) => m.OnlyZonePageModule
+      ),
   },
   {
     path: 'poll',
-    loadChildren: () => import('./pages/poll/poll.module').then( m => m.PollPageModule)
+    loadChildren: () =>
+      import('./pages/poll/poll.module').then((m) => m.PollPageModule),
   },
   {
     path: 'game1',
-    loadChildren: () => import('./pages/game1/game1.module').then( m => m.Game1PageModule)
+    loadChildren: () =>
+      import('./pages/game1/game1.module').then((m) => m.Game1PageModule),
   },
-  
+
   {
     path: '',
-    loadChildren: () => import('./pages/load-logo/load-logo.module').then( m => m.LoadLogoPageModule)
+    loadChildren: () =>
+      import('./pages/load-logo/load-logo.module').then(
+        (m) => m.LoadLogoPageModule
+      ),
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () =>
+      import('./pages/profile/profile.module').then((m) => m.ProfilePageModule),
   },
   {
     path: 'help',
-    loadChildren: () => import('./pages/help/help.module').then( m => m.HelpPageModule)
+    loadChildren: () =>
+      import('./pages/help/help.module').then((m) => m.HelpPageModule),
   },
-  
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('./pages/signup/signup.module').then((m) => m.SignupPageModule),
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/pages/signup/signup.page.spec.ts
+++ src/app/pages/signup/signup.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { SignupPage } from './signup.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('SignupPage', () => {
+  let component: SignupPage;
+  let fixture: ComponentFixture<SignupPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ SignupPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(SignupPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
