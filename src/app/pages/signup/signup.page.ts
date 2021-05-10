053e4a89fb916e6d7cc40bbb7ecd3271bcbbe667
nishant3392 <80105917+nishant3392@users.noreply.github.com>
2021-05-10 10:16:52 +0200

    update

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  signup() {
+    console.log('test');
   }
-
 }
