9230bfff1d15287d13a00cfc7bd3957cb7f0e545
nishant3392 <80105917+nishant3392@users.noreply.github.com>
2021-05-11 11:32:48 +0200

    signup connect to backend

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/model/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,11 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-  constructor() {}
+  user: User = {
+    name: '',
+    email: '',
 
-  ngOnInit() {}
+    password: '',
+  };
+  submitted = false;
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit(): void {}
 
   signup() {
-    console.log('test');
+    const data = {
+      name: this.user.name,
+      email: this.user.email,
+      mobile: this.user.mobile,
+      password: this.user.password,
+    };
+
+    this.userService.create(data).subscribe(
+      (response) => {
+        console.log(response);
+        this.submitted = true;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    this.router.navigate(['/login']);
   }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../model/user.model';
+
+const baseUrl = 'http://localhost:8080/api/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(baseUrl);
+  }
+
+  get(id: any): Observable<User> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    console.log('from frontend post: ' + data);
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
+  findByName(name: any): Observable<User[]> {
+    return this.http.get<User[]>(`${baseUrl}?name=${name}`);
+  }
+}
