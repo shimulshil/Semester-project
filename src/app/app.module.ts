198f1bfbb9de6895283d2ac7486144492224353f
shimul shil <shimul_ustc@yahoo.com>
2021-05-19 11:30:09 +0200

    pollwith-fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//-----
+
 import { GameCardComponent } from './game-card/game-card.component';
 import { GameComponent } from './game/game.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,13 +16,32 @@ import { MatIconModule } from '@angular/material/icon';
 import { RestartDialogComponent } from './restart-dialog/restart-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { NotificationsComponent } from './notifications/notifications.component';
+
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 //------
 @NgModule({
-  declarations: [AppComponent, GameCardComponent,
-    RestartDialogComponent, GameComponent, NotificationsComponent],
+  declarations: [
+    AppComponent,
+    GameCardComponent,
+    RestartDialogComponent,
+    GameComponent,
+    NotificationsComponent,
+  ],
   entryComponents: [NotificationsComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule,
-    MatToolbarModule, MatButtonModule, MatIconModule, MatDialogModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyASb8vck2wbK2XDqDDGFvtXoVo_LaW9Dqo',
+    authDomain: 'vent2u-dc7b8.firebaseapp.com',
+    projectId: 'vent2u-dc7b8',
+    storageBucket: 'vent2u-dc7b8.appspot.com',
+    messagingSenderId: '628890693152',
+    appId: '1:628890693152:web:58a3145c5b6b48363fd645',
+    measurementId: 'G-9GJVP55L8C',
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
