f406debc81ba7b4b6c87f8a32650af16912dd511
nishant3392 <80105917+nishant3392@users.noreply.github.com>
2021-05-20 18:08:27 +0200

    signup & login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 // import { MemorygameComponent } from './memorygame/memorygame.component';
 import { GameComponent } from './game/game.component';
 
-
 const routes: Routes = [
   { path: 'game', component: GameComponent },
   {
     path: '',
     redirectTo: 'opening-screen',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   // {
   //   path: 'tabs',
@@ -31,62 +30,74 @@ const routes: Routes = [
   //   loadChildren: () => import('./pages/the-tabs/tab3/tab3.module').then(m => m.Tab3PageModule)
   // },
 
-
   {
     path: 'opening-screen',
-    loadChildren: () => import('./pages/opening-screen/opening-screen.module').then( m => m.OpeningScreenPageModule)
+    loadChildren: () =>
+      import('./pages/opening-screen/opening-screen.module').then(
+        (m) => m.OpeningScreenPageModule
+      ),
   },
   {
     path: 'poll',
-    loadChildren: () => import('./pages/poll/poll.module').then( m => m.PollPageModule)
+    loadChildren: () =>
+      import('./pages/poll/poll.module').then((m) => m.PollPageModule),
   },
-
 
   {
     path: 'zona-status',
-    loadChildren: () => import('./pages/zona-status/zonastatus.module').then( m => m.zonastatusPageModule)
+    loadChildren: () =>
+      import('./pages/zona-status/zonastatus.module').then(
+        (m) => m.zonastatusPageModule
+      ),
   },
   {
     path: 'actual-status',
-    loadChildren: () => import('./pages/actual-status/actualstatus.module').then( m => m.actualstatusPageModule)
+    loadChildren: () =>
+      import('./pages/actual-status/actualstatus.module').then(
+        (m) => m.actualstatusPageModule
+      ),
   },
-
 
   {
     path: 'change-clima',
-    loadChildren: () => import('./pages/change-clima/change-clima.module').then( m => m.ChangeClimaPageModule)
+    loadChildren: () =>
+      import('./pages/change-clima/change-clima.module').then(
+        (m) => m.ChangeClimaPageModule
+      ),
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
   },
   {
     path: 'sign-up',
-    loadChildren: () => import('./pages/sign-up/sign-up.module').then( m => m.SignUpPageModule)
+    loadChildren: () =>
+      import('./pages/sign-up/sign-up.module').then((m) => m.SignUpPageModule),
   },
   {
     path: 'selection',
-    loadChildren: () => import('./pages/selection/selection.module').then( m => m.SelectionPageModule)
+    loadChildren: () =>
+      import('./pages/selection/selection.module').then(
+        (m) => m.SelectionPageModule
+      ),
   },
-  {
-    path: 'pool-result',
-    loadChildren: () => import('./pool-result/pool-result.module').then( m => m.PoolResultPageModule)
-  },
-  
+
   {
     path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () =>
+      import('./pages/profile/profile.module').then((m) => m.ProfilePageModule),
   },
   {
     path: 'help',
-    loadChildren: () => import('./pages/help/help.module').then( m => m.HelpPageModule)
+    loadChildren: () =>
+      import('./pages/help/help.module').then((m) => m.HelpPageModule),
   },
-
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/pages/sign-up/sign-up.page.ts
+++ src/app/pages/sign-up/sign-up.page.ts
@@ -1,4 +1,9 @@
+/* eslint-disable object-shorthand */
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +11,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
+  name: string;
+  email: string;
+  phone: number;
+  password: string;
+  constructor(
+    private afs: AngularFirestore,
+    private afauth: AngularFireAuth,
+    private router: Router,
+    private loadingCtrl: LoadingController,
+    private toaster: ToastController
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async signup() {
+    if (this.name && this.email && this.phone && this.password) {
+      const loading = await this.loadingCtrl.create({
+        message: 'processing..',
+        spinner: 'crescent',
+        showBackdrop: true,
+      });
+
+      loading.present();
+
+      this.afauth
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then((data) => {
+          data.user.sendEmailVerification();
+          this.afs
+            .collection('user')
+            .doc(data.user.uid)
+            .set({
+              userId: data.user.uid,
+              userName: this.name,
+              userEmail: this.email,
+              userPhone: this.phone,
+              userPassword: this.password,
+            })
+            .then(() => {
+              loading.dismiss();
+              this.toast(
+                'Registration Sucess! Please Check Your Email!',
+                'sucess'
+              );
+              this.router.navigate(['./login']);
+            })
+            .catch((error) => {
+              loading.dismiss();
+              this.toast(error.message, 'danger');
+            });
+        });
+    }
   }
 
+  async toast(message, status) {
+    const toast = await this.toaster.create({
+      message: message,
+      color: status,
+      position: 'top',
+      duration: 2000,
+    });
+    toast.present();
+  }
 }
